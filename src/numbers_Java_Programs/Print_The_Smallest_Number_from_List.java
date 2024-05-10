2734c7686d0c6c56279dbbf6b9ea4e6a089d1720
Anil <Anil@DESKTOP-7C4J88S>
2024-05-10 09:29:12 +0530

    //committing the program for smallest and largest number from array and list

---
+++ src/numbers_Java_Programs/Print_The_Smallest_Number_from_List.java
@@ -0,0 +1,27 @@
+package numbers_Java_Programs;
+
+import java.util.ArrayList;
+
+public class Print_The_Smallest_Number_from_List 
+{
+	public static void main(String[] args) {
+		
+		ArrayList <Integer> numlist = new ArrayList<Integer>();
+		
+		for (int i=55; i<=111;i++)
+		{
+			numlist.add(i);
+		}
+		int smallest=numlist.get(0);
+		
+		for(int j=0;j<numlist.size();j++)
+		{
+			if(numlist.get(j)<smallest)
+			{
+				smallest=numlist.get(j);			}
+		}
+		
+		System.out.println(smallest);
+	}
+
+}
