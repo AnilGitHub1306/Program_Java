ab64b88b902fbe1bd6792d16dcdde9e435a10012
Anil <Anil@DESKTOP-7C4J88S>
2024-05-10 08:57:55 +0530

    //committing program increase number order by 2

---
+++ src/numbers_Java_Programs/Print_Number_Incresred_order_By_2.java
@@ -0,0 +1,13 @@
+package numbers_Java_Programs;
+
+public class Print_Number_Incresred_order_By_2 
+{
+	 public static void main(String[] args) {
+		
+		 for(int i=1;i<=100;i=i+2)
+		 {
+			 System.out.println(i);
+		 }
+	}
+
+}
