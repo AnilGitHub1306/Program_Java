f473e890ad1b5719335214f47808d824352f4a30
Anil <Anil@DESKTOP-7C4J88S>
2024-05-10 08:53:53 +0530

    //Commiting print odd number from 0 to 100 programs

---
+++ src/numbers_Java_Programs/Print_Odd_Number_From_0_To_100_n.java
@@ -0,0 +1,15 @@
+package numbers_Java_Programs;
+
+public class Print_Odd_Number_From_0_To_100_n 
+{
+	public static void main(String[]args)
+	{
+		for(int i=0; i<=100; i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
